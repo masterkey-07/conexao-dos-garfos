b90905ed9d6f33deb9ac78ae156a48f485683c7e
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2025-04-29 07:56:06 -0300

    feat(graph): added add_edge function

--- fork-cli/graph/core/graph.py
+++ fork-cli/graph/core/graph.py
@@ -1,4 +1,6 @@
+from graph.core.edge import Edge
 from graph.core.node import Node
+from graph.error.node import NodeNotFoundError
 from graph.utils.node_pool import NodePool
 
 class Graph:
@@ -6,15 +8,36 @@ class Graph:
         self._edges = []
         self._node_pool = NodePool()
 
-    def add_node(self, id:str, properties:dict = None) -> bool:
+    def add_node(self, id:str, properties:dict = None):
         new_node = Node(id, properties)
         
-        self._node_pool.set(new_node)
+        try:
+            self._node_pool.set(new_node)
+
+            return new_node
+        except:
+            return None
 
     def get_node(self, id:str):
         return self._node_pool.get(id)
     
     def delete_node(self, id:str):
-        return self._node_pool.delete(id)
+        try:
+            self._node_pool.delete(id)
+        except:
+            return False
+
+        return True
     
-    
+    def add_edge(self, first_node_id:str, second_node_id:str, properties = None):
+        first_node = self._node_pool.get(first_node_id)
+        second_node = self._node_pool.get(second_node_id)
+        
+        if first_node is None or second_node is None:
+            return None
+
+        edge = Edge(first_node, second_node, properties)
+
+        self._edges.append(edge)
+
+        return edge
--- fork-cli/test/graph/core/graph_test.py
+++ fork-cli/test/graph/core/graph_test.py
@@ -1,29 +1,56 @@
 import pytest
-
+from graph.core.edge import Edge
+from graph.core.node import Node
 from graph.core.graph import Graph
+from graph.error.node import NodeNotFoundError
 
-def test_is_buildable():
-    _ = Graph()
+@pytest.fixture
+def graph():
+    return Graph()
 
-def test_add_node():
-    fork = Graph()
+def test_add_node(graph: Graph):
+    node = graph.add_node("A", {"color": "red"})
+    assert node is not None
+    assert isinstance(node, Node)
+    assert node.id == "A"
+    assert node.properties == {"color": "red"}
+
+def test_get_node_existing(graph: Graph):
+    graph.add_node("B", {"value": 5})
+    node = graph.get_node("B")
+    assert node is not None
+    assert node.id == "B"
+    assert node.properties["value"] == 5
+
+def test_get_node_nonexistent(graph: Graph):
+    assert graph.get_node("Z") is None
+
+def test_delete_node(graph: Graph):
+    graph.add_node("C")
+    deleted = graph.delete_node("C")
+    assert deleted is True
+    assert graph.get_node("C") is None
+
+def test_delete_node_nonexistent(graph: Graph):
+    deleted = graph.delete_node("X")
     
-    props = {'weigh': 10}
+    print(deleted)
 
-    fork.add_node('A', props)
+    assert deleted is False
 
-    node = fork.get_node('A')
+def test_add_edge_success(graph: Graph):
+    graph.add_node("A")
+    graph.add_node("B")
+    edge = graph.add_edge("A", "B", {"weight": 3})
+    assert edge is not None
+    assert isinstance(edge, Edge)
+    assert edge.first_node.id == "A"
+    assert edge.second_node.id == "B"
+    assert edge.properties["weight"] == 3
 
-    assert node.id == 'A'
-    assert node.properties == props
-
-def test_delete_node():
-    fork = Graph()
-
-    fork.add_node('A')
-
-    fork.delete_node('A')
-
-    node = fork.get_node('A')
-
-    assert node == None
+def test_add_edge_with_missing_node(graph: Graph):
+    graph.add_node("A")
+    
+    edge = graph.add_edge("A", "B")
+    
+    assert edge is None
