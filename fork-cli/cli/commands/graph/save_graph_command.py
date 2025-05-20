cb7cc33c3788aba5c00a74a58ef4518a29e5e583
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2025-05-20 20:42:32 -0300

    feat: implement SaveGraphCommand to enable saving the current graph to the selected project

---
+++ fork-cli/cli/commands/graph/save_graph_command.py
@@ -0,0 +1,21 @@
+from cli.command import Command
+
+class SaveGraphCommand(Command):
+    @property
+    def symbol(self) -> str:
+        return "sgv"
+
+    def execute(self, context, args):
+        if not hasattr(context, "current_project") or context.current_current_project is None:
+            print("No project selected. Please select a project first.")
+            return
+
+        if not hasattr(context, "current_graph") or context.current_graph is None:
+            print("No graph selected. Please select a graph first.")
+            return
+
+        context.current_project.save_graph(context.current_graph)
+        print(f"Graph '{context.current_graph.name}' saved in project '{context.current_project.project_name}'.")
+
+    def __str__(self):
+        return "sgv - Save the current graph to the current project"
